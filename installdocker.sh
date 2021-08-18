1c7491759748c9ff352aabe72be6e315103946ae
joaovitorit <63624977+joaovitorit@users.noreply.github.com>
2021-08-18 12:33:35 -0300

    Instalar a stack com docker compose linhas 16,17

--- installdocker.sh
+++ installdocker.sh
@@ -13,6 +13,8 @@ echo "############## APLICANDO ATALHO NO EXECUTáVEL #############"
 ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 echo "############## VERSÃO DO DOCKER-COMPOSE #############"
 docker-compose version
+echo "############## INSTALANDO A STACK DO ZABBIX #############"
+docker-compose up -d
 
 
 

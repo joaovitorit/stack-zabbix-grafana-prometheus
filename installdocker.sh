93061a0b0c96c4e92478c6307d84b5927dcb47d1
joaovitorit <63624977+joaovitorit@users.noreply.github.com>
2021-08-18 11:59:27 -0300

    Update installdocker.sh

--- installdocker.sh
+++ installdocker.sh
@@ -9,7 +9,11 @@ echo "############## INICIANDO A INSTALAÇÀO DOCKER-COMPOSE #############"
 curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 echo "############## APLICANDO PERMISSÃO NO EXECUTáVEL #############"
 chmod +x /usr/local/bin/docker-compose
-cho "############## APLICANDO ATALHO NO EXECUTáVEL #############"
+echo "############## APLICANDO ATALHO NO EXECUTáVEL #############"
 ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 echo "############## VERSÃO DO DOCKER-COMPOSE #############"
 docker-compose version
+echo "############## INSTALANDO A STACK DO ZABBIX #############"
+docker-compose up -d
+
+
